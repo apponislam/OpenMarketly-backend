40dbf16f5b801b3a1a954f14d4de10e6c1480baa
Appon Islam <appon2003@gmail.com>
2026-08-13 12:46:10 +0600

    new vercel link added

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import config from "./app/config";
 const app: Application = express();
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "http://localhost:3066"],
+    origin: ["http://localhost:3000", "http://localhost:3066", "https://open-marketly-frontend-web.vercel.app", "http://open-marketly-frontend-web.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
